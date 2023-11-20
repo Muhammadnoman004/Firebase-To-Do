88da3be54ca680ed0e8891ccd18f9c55087a2843
Muhammadnoman004 <143292643+Muhammadnoman004@users.noreply.github.com>
2023-11-20 19:11:38 +0500

    Second commit

--- app.js
+++ app.js
@@ -7,29 +7,32 @@ getaddbtn.addEventListener("click" , ()=>{
 
     let getli = document.createElement("li");
     getli.className = "list"
+    let liTextDiv = document.createElement("div");
+    liTextDiv.setAttribute("class", "liTextDiv")
+    let liTextPara = document.createElement("p");
     let litext = document.createTextNode(getinp.value);
-    getli.appendChild(litext);
+
+    liTextPara.appendChild(litext);
+    liTextDiv.appendChild(liTextPara);
+    getli.appendChild(liTextDiv)
     getul.appendChild(getli);
     getinp.value = ""
 
     let div = document.createElement("div");
-    let span = document.createElement("span");
     div.classList.add("divClass");
     
     let editIcon = document.createElement("i");
     editIcon.className = 'edit'
     editIcon.classList.add("fa");   
     editIcon.classList.add("fa-pencil-square");
-    span.appendChild(editIcon);
-    div.appendChild(span);
 
     
     let delIcon = document.createElement("i");
     delIcon.className = "deleted"
     delIcon.classList.add("fa");
     delIcon.classList.add("fa-trash");
-    span.appendChild(delIcon);
-    div.appendChild(span);
+    div.appendChild(editIcon)
+    div.appendChild(delIcon)
     getli.appendChild(div);
 
 
